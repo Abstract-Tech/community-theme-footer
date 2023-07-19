0a80f5463598ca60fd5d0d768cc001b30d4abb65
Ivan Urdenko <vaniaurdenko@gmail.com>
2023-07-19 13:29:02 +0200

    changed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,8 @@ import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
 import { sendTrackEvent } from '@edx/frontend-platform/analytics';
 import { ensureConfig } from '@edx/frontend-platform/config';
 import { AppContext } from '@edx/frontend-platform/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 import messages from './Footer.messages';
 import LanguageSelector from './LanguageSelector';
@@ -42,25 +44,50 @@ class SiteFooter extends React.Component {
     } = this.props;
     const showLanguageSelector = supportedLanguages.length > 0 && onLanguageSelected;
     const { config } = this.context;
-
+  
     return (
       <footer
         role="contentinfo"
-        className="footer d-flex border-top py-3 px-4"
+        className="site-footer footer border-top py-3 px-4"
       >
-        <div className="container-fluid d-flex">
+        <div className="container-fluid d-flex container-footer">
           <a
             className="d-block"
             href={config.LMS_BASE_URL}
             aria-label={intl.formatMessage(messages['footer.logo.ariaLabel'])}
           >
             <img
-              style={{ maxHeight: 45 }}
+              className="footer-logo"
               src={logo || config.LOGO_TRADEMARK_URL}
               alt={intl.formatMessage(messages['footer.logo.altText'])}
             />
           </a>
-          <div className="flex-grow-1" />
+          
+          <div className="footer-section about">
+            <h2>About us</h2>
+            We help web agencies, institutions and organizations to make the best choices for their online business by selecting, designing and customizing open source web content management software. But there’s something more important that we would like to show you. Find out here
+          </div>
+  
+          <div className="footer-section contact-info">
+            <h2>Contact</h2>
+            <div>Abstract-Technology GmbH</div>
+            <div>Manfred-von-Richthofen-Straße 4 - IV. OG,</div>
+            <div>D–12101 Berlin</div>
+            <div>+49 30 214 611 08</div>
+            <div>+49 176 747 25 686</div>
+            <div>info@abstract-technology.de</div>
+          </div>
+  
+          <div className="footer-section social">
+            <h2>Follow us on social media</h2>
+            <a href="https://twitter.com/abstractGmbH" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faTwitter} size="2x" />
+            </a>
+            <a href="https://www.linkedin.com/company/abstract-technology-gmbh/" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </a>
+          </div>
+          
           {showLanguageSelector && (
             <LanguageSelector
               options={supportedLanguages}
@@ -70,7 +97,7 @@ class SiteFooter extends React.Component {
         </div>
       </footer>
     );
-  }
+  }  
 }
 
 SiteFooter.contextType = AppContext;
