08403a3d0fa21881031c2127727b1290f0473a8e
Ivan Urdenko <vaniaurdenko@gmail.com>
2023-07-21 12:40:30 +0200

    changed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -46,56 +46,58 @@ class SiteFooter extends React.Component {
     const { config } = this.context;
   
     return (
-      <footer
-        role="contentinfo"
-        className="site-footer footer border-top py-3 px-4"
-      >
-        <div className="container-fluid d-flex container-footer">
-          <a
-            className="d-block"
-            href={config.LMS_BASE_URL}
-            aria-label={intl.formatMessage(messages['footer.logo.ariaLabel'])}
-          >
-            <img
-              className="footer-logo"
-              src={logo || config.LOGO_TRADEMARK_URL}
-              alt={intl.formatMessage(messages['footer.logo.altText'])}
-            />
-          </a>
-          
-          <div className="footer-section about">
-            <h2>About us</h2>
-            We help web agencies, institutions and organizations to make the best choices for their online business by selecting, designing and customizing open source web content management software. But there’s something more important that we would like to show you. Find out here
+      <footer role="contentinfo" class="site-footer">
+          <div class="container-fluid">
+            <div class="footer-content">
+              <div class="footer-upper">
+                <div class="footer-buttons">
+                  <a href="#">What we do</a>
+                  <a href="#">Donate</a>
+                  <a href="#">Help</a>
+                  <a href="#">Contact</a>
+                  <a href="#">Honor code</a>
+                </div>
+                <div>
+                  <div class="footer-logos">
+                  <a href="https://twitter.com/abstractGmbH" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faTwitter} size="2x" color="black" />
+                  </a>
+                  <a href="https://www.linkedin.com/company/abstract-technology-gmbh/" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faLinkedin} size="2x" color="black" />
+                  </a>
+                  </div>
+                  <div class="footer-rights">
+                    © Community-theme | All rights reserved | Privacy | Terms
+                  </div>
+                </div>
+              </div>
+              <div class="footer-divider"></div>
+              <div class="footer-lower">
+                <div>
+                <a
+                    className="d-block"
+                    href={config.LMS_BASE_URL}
+                    aria-label={intl.formatMessage(messages['footer.logo.ariaLabel'])}
+                  >
+                    <img
+                      className="footer-logo"
+                      src={logo || config.LOGO_TRADEMARK_URL}
+                      alt={intl.formatMessage(messages['footer.logo.altText'])}
+                    />
+                  </a>
+                  <div class="footer-license">
+                    Theme licensed under the AGPLv3 License. Copyright 2022 by OpenCraft & Abstract Technology
+                  </div>
+                </div>
+                <div class="footer-text">
+                  edX, Open edX and their respective logos are registered trademarks of edX Inc. Free online courses at edX.org
+                </div>
+              </div>
+            </div>
           </div>
-  
-          <div className="footer-section contact-info">
-            <h2>Contact</h2>
-            <div>Abstract-Technology GmbH</div>
-            <div>Manfred-von-Richthofen-Straße 4 - IV. OG,</div>
-            <div>D–12101 Berlin</div>
-            <div>+49 30 214 611 08</div>
-            <div>+49 176 747 25 686</div>
-            <div>info@abstract-technology.de</div>
-          </div>
-  
-          <div className="footer-section social">
-            <h2>Follow us on social media</h2>
-            <a href="https://twitter.com/abstractGmbH" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faTwitter} size="2x" />
-            </a>
-            <a href="https://www.linkedin.com/company/abstract-technology-gmbh/" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faLinkedin} size="2x" />
-            </a>
-          </div>
-          
-          {showLanguageSelector && (
-            <LanguageSelector
-              options={supportedLanguages}
-              onSubmit={onLanguageSelected}
-            />
-          )}
-        </div>
-      </footer>
+        </footer>
+
+    
     );
   }  
 }
